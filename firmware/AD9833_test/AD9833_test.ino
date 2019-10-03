0e7eeba75d2cb293ad5b25907c05786a02838539
dBarmpxkos <dbarmpakos@outlook.com>
2019-10-03 21:43:45 +0300

    Add serial switch for waveform/electronics settings

--- firmware/AD9833_test/AD9833_test.ino
+++ firmware/AD9833_test/AD9833_test.ino
@@ -10,63 +10,6 @@ void setup(){
 
 void loop(){
 
-    StaticJsonDocument<200> JSONSettings;
-
-    while (!Serial.available()){
-        delay(50);
-        blink_LED();
-    }
-
-    DeserializationError error = deserializeJson(JSONSettings, Serial);
-    if (error) {
-        Serial.print(F("deserializeJson() failed: "));
-        Serial.println(error.c_str());
-        return;
-    }
-
-    bool enable = JSONSettings["en"];
-
-    if (!enable)
-        gen.EnableOutput(enable);
-    
-    else {
-        
-        Registers outputRegister;
-        unsigned int incomingRegister;
-        switch ( incomingRegister ) {
-            case 0:
-                outputRegister = REG0;
-                break;
-            case 1:
-                outputRegister = REG1;
-                break;  
-            default:
-                Serial.println(F("Error in register setting"));
-                break;                    
-        } 
-
-        WaveformType waveType;
-        unsigned int incomingWave;
-        switch ( incomingWave ) {
-            case 0:
-                waveType = SINE_WAVE;
-                break;
-            case 1:
-                waveType = TRIANGLE_WAVE;
-                break;
-            case 2:
-                waveType = SQUARE_WAVE;
-                break;  
-            default:
-                Serial.println(F("Error in waveform setting"));
-                break;                    
-        } 
-
-        float frequency = JSONSettings["freq"];
-        float phase     = JSONSettings["phase"];
-        gen.SetOutputSource(outputRegister);
-        gen.ApplySignal(waveType, outputRegister, frequency, 
-                        outputRegister, phase);
-    }
-
+if (monitor_serial() == 0)
+    waveform_settings();
 }
--- firmware/AD9833_test/functions.h
+++ firmware/AD9833_test/functions.h
@@ -23,99 +23,88 @@ static void flush_serial_input(void){
 static void blink_LED(void){
     digitalWrite(LED_PIN, digitalRead(LED_PIN) == HIGH ? LOW : HIGH);
 }
-/* helper functions */
 
-/*
- * Setup a manual ramp from a Start frequency to a Stop frequency in some increment
- * over a ramp time. 
- */
-void IncrementFrequencyTest (const float startHz, const float stopHz, const float incHz, const float sweepTimeSec ) {
- 
-    // Calculate the delay between each increment.
-    uint16_t numMsecPerStep = (sweepTimeSec * 1000.0) / ((uint16_t)((stopHz - startHz) / incHz) + 1);
-    if ( numMsecPerStep == 0 ) numMsecPerStep = 1;
+int8_t monitor_serial(void) {
 
-    // Apply a signal to the output. If phaseReg is not supplied, then
-    // a phase of 0.0 is applied to the same register as freqReg
-    gen.ApplySignal(SINE_WAVE,REG1,startHz);
+    int8_t settingsType = -1;
+    if (Serial.available() > 0) {
 
-    while ( true ) {
-      
-        gen.SetFrequency(REG1,startHz-incHz);
+        char cmd = Serial.read();
+        if (cmd == '>') {
+            cmd = Serial.read();
 
-        for ( float i = startHz ; i <= stopHz; i += incHz ) {
-            yield_on_char();
-            gen.IncrementFrequency(REG1,incHz);
-            delay(numMsecPerStep); 
+            switch (cmd) {
+                case 'W':
+                    settingsType = 0;
+                    break;
+                case 'S':
+                    settingsType = 1;
+                    break;
+                default:
+                    Serial.println(F("Error in settings type"));
+                    break;
+            }
         }
+    }
+    return settingsType;
+}
+
+bool waveform_settings(void) {
+    StaticJsonDocument<200> JSONSettings;
+
+    DeserializationError error = deserializeJson(JSONSettings, Serial);
+    if (error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.c_str());
+        return;
+    }
+    bool enable = JSONSettings["en"];
+
+    if (!enable)
+        gen.EnableOutput(false);
+    
+    else {
+        Registers outputRegister;
+        unsigned int incomingRegister;
+        switch ( incomingRegister ) {
+            case 0:
+                outputRegister = REG0;
+                break;
+            case 1:
+                outputRegister = REG1;
+                break;  
+            default:
+                Serial.println(F("Error in register setting"));
+                break;                    
+        } 
+
+        WaveformType waveType;
+        unsigned int incomingWave;
+        switch ( incomingWave ) {
+            case 0:
+                waveType = SINE_WAVE;
+                break;
+            case 1:
+                waveType = TRIANGLE_WAVE;
+                break;
+            case 2:
+                waveType = SQUARE_WAVE;
+                break;  
+            default:
+                Serial.println(F("Error in waveform setting"));
+                break;                    
+        } 
+
+        float frequency = JSONSettings["freq"];
+        float phase     = JSONSettings["phase"];
+        gen.ApplySignal(waveType, outputRegister, frequency, 
+                        outputRegister, phase);
+        gen.SetOutputSource(outputRegister);
+
     }
 }
 
-/*
- * Cycle through all of the waveform types. Also cycle the 
- * frequency registers.
- */
-void CycleWaveformsTest ( void ) {
-  
-    WaveformType waveType = SINE_WAVE;
-    gen.SetFrequency(REG0,10000.0);   // Load values
-    gen.SetFrequency(REG1,1000.0);
-    // We don't care about phase for this test
-    
-    while ( true ) {
-
-        gen.SetWaveform(REG1,waveType);   // Next waveform
-        gen.SetWaveform(REG0,waveType);
-        gen.SetOutputSource(REG1);        // Output 1000 Hz waveform
-
-        // Hack to allow I'm alive lamp a chance to blink and give a better
-        // response to user input
-        
-        gen.SetOutputSource(REG0);        // Output 10000 Hz waveform
-        
-
-        switch ( waveType ) {             // Cycle through all the waveform types
-            case SINE_WAVE:
-                waveType = TRIANGLE_WAVE;
-                break;
-            case TRIANGLE_WAVE:
-                waveType = SQUARE_WAVE;
-                break;
-            case SQUARE_WAVE:
-                waveType = HALF_SQUARE_WAVE;
-                break;
-            case HALF_SQUARE_WAVE:
-                waveType = SINE_WAVE;
-                break; 
-        }
-    }    
-}
-
-
-/*
- * Fast switching example.
- * I use the FFT display capability on my scope
- */
-void SwitchFrequencyRegisterTest ( void ) {
-
-    gen.ApplySignal(SINE_WAVE,REG0,500000);
-    gen.ApplySignal(SINE_WAVE,REG1,100000);
-    gen.SetPhase(REG1,180);           // Offset second freq by 180 deg
-    gen.Reset();
-
-    while ( true ) {                  // This takes time
-        
-        yield_on_char();                 // This takes more time
-
-        gen.SetOutputSource(REG0);    // This takes about 18 usec
-        gen.SetOutputSource(REG1);    // This takes about 18 usec  
-        
-        // What ends up is REG0 frequency is active a shorter amount of time
-        // then REG1 frequency. In the sepctrum, the duty cycle differences will
-        // show up (power is lower by 10log(DC))
-    }  
-}
-
+/* helper functions */
 
 /*
  * Show the requested versus actual programmed values for frequency and phase
@@ -153,33 +142,3 @@ void RequestedvsProgrammedValues ( void ) {
         Serial.println(buffer);       
     }
 }
-
-/*
-bool serial_switcher(const char choice){
-    switch ( choice ) {
-        case '1':
-            IncrementFrequencyTest();
-            break;
-        case '2':
-            CycleWaveformsTest();
-            break;  
-        case '3':
-            SwitchFrequencyRegisterTest();
-            break; 
-        case '4':
-            PhaseTest();
-            break;
-        case '5':
-            RequestedvsProgrammedValues();
-            break;
-        case '6':
-            outputOn = ! outputOn;
-            gen.EnableOutput(outputOn);    // Turn off output
-            break;
-        default:
-            Serial.println(F("*** Invalid command ***"));
-            break;                    
-    } 
-}
-*/
-/* helper functions */
